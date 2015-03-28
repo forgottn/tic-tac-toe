87a1ee269685a19992a66c07facd2251b2a56e8d
forgottn <pduong@berkeley.edu>
2015-03-27 18:20:14 -0700

    Removed unnecessary selector helper

--- features/step_definitions/tic_tac_toe_step.rb
+++ features/step_definitions/tic_tac_toe_step.rb
@@ -1,8 +1,6 @@
 require 'uri'
 require 'cgi'
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
-require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "selectors"))
-
 
 Given /^(?:|I )am on (.+)$/ do |page_name|
   visit path_to(page_name)
